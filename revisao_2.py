f3e4daa05e32e31db64cd7c7b4f0e84999e95bef
ameliezices <carvalho.lima@ufms.br>
2026-03-12 10:44:28 -0400

    Add files via upload

--- revisao_2.py
+++ revisao_2.py
@@ -1,1 +1,1 @@
-#Programa que 
+#Programa que lê dois vetores, com 10 elementos cada. Gere um terceiro vetor de 20 elementos, cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
