8a7233cff2aae611a3d305185416a4ad43673c01
ameliezices <carvalho.lima@ufms.br>
2026-03-12 10:36:50 -0400

    Remove code for finding the largest of three numbers

--- revisao_2.py
+++ revisao_2.py
@@ -1,9 +1,1 @@
-#Programa que, dados três números inteiros, imprime o maior deles.
-a = print(input("Digite um número: "))
-b = print(input("Digite um número: "))
-c = print(input("Digite um número: "))
 
-if a >= b and a >= c:
-    print(f"O maior número fornecido é {a}")
-elif b >= c:
-    print(f"O maior número fornecido é {b}")
